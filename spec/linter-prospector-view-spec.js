e35b391791b86415e0e797dd4fdb6180a9216350
Carlos Coelho <carlos@vinta.com.br>
2017-10-18 18:55:35 -0300

    Added base atom package structure
    Signed-off-by: Carlos Coelho <carlos@vinta.com.br>

---
+++ spec/linter-prospector-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LinterProspectorView from '../lib/linter-prospector-view';
+
+describe('LinterProspectorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
